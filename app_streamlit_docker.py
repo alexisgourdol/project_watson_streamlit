7c8e4cba81a09b04436fd1550b70509fccd07e86
Stefania Apuzzo <stefania.apuzzo@gmail.com>
2020-09-15 17:06:37 +0200

    creating an app de ouf

--- app_streamlit_docker.py
+++ app_streamlit_docker.py
@@ -1,4 +1,6 @@
 from transformers import BertTokenizer, TFBertModel
+import requests
+import json
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -27,49 +29,67 @@ def user_input_language():
         )
     return idiom
 
-def two_sentences(idiom):
-    data = pd.read_csv("project_watson/data/train.csv")
-    premise = data.premise[data.language == idiom].reset_index(drop=True)[np.random.randint(0,len(idiom))]
-    hypothesis = data[data['premise'] == premise].hypothesis.values[0]
-    return premise, hypothesis
-
-premise = premise
-hypothesis = hypothesis
-model_name = 'bert-base-multilingual-cased'
-tokenizer = BertTokenizer.from_pretrained(model_name)
+# def two_sentences(idiom):
+#     data = pd.read_csv("project_watson/data/train.csv")
+#     premise = data.premise[data.language == idiom].reset_index(drop=True)[np.random.randint(0,len(idiom))]
+#     hypothesis = data[data['premise'] == premise].hypothesis.values[0]
+#     return premise, hypothesis
 
 def encode_sentence(s):
-   tokens = list(tokenizer.tokenize(s))
-   tokens.append('[SEP]')
-   return tokenizer.convert_tokens_to_ids(tokens)
+    """ Encode One sentence s using the tokenizer defined in this .py"""
+    tokens = list(tokenizer.tokenize(s))
+    tokens.append('[SEP]')
+    return tokenizer.convert_tokens_to_ids(tokens)
 
 def bert_encode(hypotheses, premises, tokenizer):
+    """Returns a mathematical representation of the text inputs using the defined
+    tokenizer. The model expects 'input_word_ids', 'input_mask', 'input_type_ids'"""
 
-  sentence1 = tf.ragged.constant([encode_sentence(s) for s in np.array(hypotheses)])
-  sentence2 = tf.ragged.constant([encode_sentence(s) for s in np.array(premises)])
+    sentence1 = tf.ragged.constant([encode_sentence(s) for s in np.array(hypotheses)])
+    sentence2 = tf.ragged.constant([encode_sentence(s) for s in np.array(premises)])
 
-  cls = [tokenizer.convert_tokens_to_ids(['[CLS]'])]*sentence1.shape[0]
-  input_word_ids = tf.concat([cls, sentence1, sentence2], axis=-1)
+    cls = [tokenizer.convert_tokens_to_ids(['[CLS]'])]*sentence1.shape[0]
+    input_word_ids = tf.concat([cls, sentence1, sentence2], axis=-1)
 
-  input_mask = tf.ones_like(input_word_ids).to_tensor()
+    input_mask = tf.ones_like(input_word_ids).to_tensor()
 
-  type_cls = tf.zeros_like(cls)
-  type_s1 = tf.zeros_like(sentence1)
-  type_s2 = tf.ones_like(sentence2)
-  input_type_ids = tf.concat(
-      [type_cls, type_s1, type_s2], axis=-1).to_tensor()
+    type_cls = tf.zeros_like(cls)
+    type_s1 = tf.zeros_like(sentence1)
+    type_s2 = tf.ones_like(sentence2)
+    input_type_ids = tf.concat([type_cls, type_s1, type_s2], axis=-1).to_tensor()
 
-  inputs = {
-      'input_word_ids': input_word_ids.to_tensor(),
+    inputs = {'input_word_ids': input_word_ids.to_tensor(),
       'input_mask': input_mask,
       'input_type_ids': input_type_ids}
 
-  return inputs
+    return inputs
 
-test_input = bert_encode(premise, hypothesis, tokenizer)
-predictions = [np.argmax(i) for i in model.predict(test_input)]
+def format_input(test, tokenizer):
+    """Reduces #tokens to 50 (max_len), as expected by the model to predict."""
+    test_input = bert_encode(test.premise.values, test.hypothesis.values, tokenizer)
+
+    # take the 1st 50 tokens of the 20 first lines and convert to list. Why 20 ? No reason, could be 1...
+    test_input_slice = {'input_word_ids': tf.slice(test_input['input_word_ids'], [0, 0], [1, 50]).numpy().tolist(),
+                        'input_mask' : tf.slice(test_input['input_mask'], [0, 0], [1, 50]).numpy().tolist(),
+                        'input_type_ids' : tf.slice(test_input['input_type_ids'], [0, 0], [1, 50]).numpy().tolist()}
+
+    #format data as expected by the model for a prediction AND KEEP ONLY THE 1ST LINE (TO DO : refactor)
+    # ==> {'instances': [{'input_word_ids': [101, 10111, ... 11762] , 'input_mask': [1, 1, ... 1], 'input_type_ids': [0, 0, ... 0]
+
+    data = {}
+    for k in test_input_slice.keys():
+        data[k] = test_input_slice[k][0]
+
+    data = {'instances': [data]}
+
+    return data
+
+# def predict(data, url=URL_PREDICTION ):
+#     r = requests.post(url, data=json.dumps(data))
+#     return r.text
 
 def main():
+
     #TITLE
     st.write("""
     # Project Watson
@@ -80,17 +100,29 @@ def main():
     idiom = user_input_language()
 
     #INPUT PARAMETERS
-    st.subheader('User Input parameters')
-    df_two_sentences = pd.DataFrame(two_sentences(idiom), index=["premise", "hypothesis"]).T
-    st.table(df_two_sentences.assign(hack="").set_index("hack"))
+    st.subheader('Your turn!')
+    user_premise = st.text_area("Your premise here")
+    user_hypothesis = st.text_area("Your hypothesis here")
+    data = {'premise': [user_premise], 'hypothesis': [user_hypothesis]}
+    df_two_sentences = pd.DataFrame(data=data)
+    st.table(df_two_sentences)
 
     #CLASS LABELS
-    st.subheader('Class labels')
-    st.write(pd.DataFrame(data={
-        "label" : [0,1,2],
-        "evaluation" : ["Entailment","Neutral","Contradiction"]
-        }).set_index("evaluation"))
-    st.write(predictions)
+    # st.subheader('Class labels')
+    # st.write(pd.DataFrame(data={
+    #     "label" : [0,1,2],
+    #     "evaluation" : ["Entailment","Neutral","Contradiction"]
+    #     }).set_index("evaluation"))
+    # st.write(predictions)
+
+    test = df_two_sentences
+
+    embedded_s = format_input(test, tokenizer)
+
+    # predict(df)
+    st.write(pd.DataFrame(data=embedded_s))
 
 if __name__ == "__main__":
+    model_name = 'bert-base-multilingual-cased'
+    tokenizer = BertTokenizer.from_pretrained(model_name)
     main()
